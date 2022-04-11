c96967844965d7abef8d6627c5b094be4b45598e
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-04-11 20:42:06 +0300

    a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a Python script that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    Employee_id = sys.argv[1]
+    user = '{}users/{}'.format(url, Employee_id)
+    json_obj = requests.get(user).json()
+
+    todo = '{}todos?userId={}'.format(url, Employee_id)
+    tasks = requests.get(todo).json()
+
+    com_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            com_task.append(task.get('title'))
+
+    print('Employee {} is done with tasks({}/{}):'.format(json_obj.get('name'),
+          len(com_task), len(tasks)))
+    for com in com_task:
+        print(com)
