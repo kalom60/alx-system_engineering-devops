1397cb08788c6561d26245ddcfa6ca505be5657a
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-04-11 23:59:24 +0300

    request inside dump

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,27 +7,18 @@ import json
 import requests
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get("{}users".format(url)).json()
 
-    users = requests.get('{}users'.format(url)).json()
-    records = {}
-    name = {}
-
-    for user in users:
-        Employee_id = user.get("id")
-        records[Employee_id] = []
-        name[Employee_id] = user.get("username")
-
-    todos = requests.get('{}todos'.format(url)).json()
-    for todo in todos:
-        Employee_id = todo.get("userId")
-        todo_obj = {
-            "username": name.get(Employee_id),
-            "task": todo.get('title'),
-            "completed": todo.get('completed')
-        }
-        records.get(Employee_id).append(todo_obj)
-
-    filename = 'todo_all_employees.json'
-    with open(filename, 'w') as file:
-        json.dump(records, file)
+    filename = "todo_all_employees.json"
+    with open(filename, "w") as file:
+        json.dump({
+            user.get("id"): [{
+                "username": user.get("username"),
+                "task": task.get("title"),
+                "completed": task.get("completed")
+            } for task in requests.get("{}todos".format(url),
+                                       params={
+                                           "userId": user.get("id")
+            }).json()]
+            for user in users}, file)
