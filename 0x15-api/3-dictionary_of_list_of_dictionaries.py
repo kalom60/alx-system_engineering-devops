006fa39c676a03e91b7b951af18766bfffa23f2f
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-04-11 23:29:17 +0300

    fix error

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,24 +9,24 @@ import requests
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
 
-    users = requests.get("{}users".format(url)).json()
-    tasks = requests.get("{}todos".format(url)).json()
+    users = requests.get('{}users'.format(url)).json()
     records = {}
+    name = {}
 
     for user in users:
         Employee_id = user.get("id")
-        name = user.get("username")
+        records[Employee_id] = []
+        name[Employee_id] = user.get("username")
 
-        for task in tasks:
-            if (task.get("userId") == int(Employee_id)):
-                user_dict = {
-                    "task": task.get("title"),
-                    "completed": task.get("completed"),
-                    "username": name
-                }
-                tasks.append(user_dict)
-
-        records[Employee_id] = tasks
+    todos = requests.get('{}todos'.format(url)).json()
+    for todo in todos:
+        Employee_id = todo.get("userId")
+        todo_obj = {
+            "task": todo.get('title'),
+            "completed": todo.get('completed'),
+            "username": name.get(Employee_id)
+        }
+        records.get(Employee_id).append(todo_obj)
 
     filename = 'todo_all_employees.json'
     with open(filename, 'w') as file:
