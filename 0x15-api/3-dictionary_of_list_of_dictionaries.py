06a29310be1854dbe31f7a0736c640bad526ab5c
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-04-12 00:08:51 +0300

    Records all tasks from all employees

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
-"""
-    Python script to export data in the JSON format
-    Records all tasks from all employees
-"""
+"""This script will extract information and save it in json file"""
 import json
 import requests
 
-
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-    user = '{}users'.format(url)
-    res = requests.get(user)
-    json_obj = res.json()
-    record = {}
-
-    for user in json_obj:
-        name = user.get('username')
-        Employee_id = user.get('id')
-
-        todos = '{}todos?userId={}'.format(url, Employee_id)
-        res = requests.get(todos)
-        tasks = res.json()
-        all_task = []
-
-        for task in tasks:
-            task_obj = {"username": name,
-                        "task": task.get('title'),
-                        "completed": task.get('completed')}
-            all_task.append(task_obj)
-
-        record[str(Employee_id)] = all_task
-
-    filename = 'todo_all_employees.json'
-    with open(filename, 'w') as file:
-        json.dump(record, file)
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+    user = requests.get("https://jsonplaceholder.typicode.com/users")
+    json_todo = todo.json()
+    json_user = user.json()
+    todo_list = json_todo[:]
+    user_list = json_user[:]
+    id_list = []
+    for i in todo_list:
+        id_list.append(i)
+    user_name = []
+    for i in user_list:
+        user_name.append(i)
+    for i in user_name:
+        for j in id_list:
+            if j['userId'] == i['id']:
+                del j['id']
+                j['task'] = j['title']
+                del j['title']
+                j['username'] = j['userId']
+                j['username'] = i['username']
+    for i in id_list:
+        del i['userId']
+    i = 0
+    list_of_list = []
+    while (i < len(id_list)):
+        list_of_list.append(id_list[i:i+20])
+        i += 20
+    file_name = "todo_all_employees.json"
+    with open(file_name, "w", encoding='utf-8') as f:
+        dic = {}
+        for i in list_of_list:
+            for j in i:
+                for k in user_name:
+                    if j['username'] == k['username']:
+                        dic[str(k['id'])] = i
+        f.write(json.dumps(dic))
