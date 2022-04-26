6e346fdc933f73f8598779c97d5754e3e456bb27
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-04-27 02:48:01 +0300

    pep8

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -39,7 +39,7 @@ def count_words(subreddit, word_list):
     """If no posts match or the subreddit is invalid, print nothing"""
     rec = recurse(subreddit)
     if rec is None:
-        return 
+        return
     count = {word: 0 for word in word_list}
     for i in rec:
         for word in word_list:
