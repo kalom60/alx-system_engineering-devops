4af3e3bc8ea3e4c86337db108956477ecb4e0c1f
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-04-27 23:17:08 +0300

    fix documentation error

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,28 +7,6 @@ of all hot articles, and prints a sorted count of given keywords
 import requests
 
 
-def hot_print(res, word_list, param, hot_dict):
-    """
-    count the repeated words
-    """
-    top = res.json()["data"].get("children")
-    for hot in top:
-        for hot_word in word_list:
-            title = hot.get("data").get("title")
-            if title is not None:
-                words = title.split()
-                for word in words:
-                    if hot_word.lower() == word.lower():
-                        hot_dict[hot_word] += 1
-
-    if param is None:
-        count = sorted(hot_dict.items(), key=lambda x: x[1], reverse=True)
-
-        for k, v in count:
-            if v != 0:
-                print(f'{k}: {v}')
-
-
 def count_words(subreddit, word_list, param1=None, hot_dict={}):
     """
     If no posts match or the subreddit is invalid, print nothing.
@@ -52,11 +30,49 @@ def count_words(subreddit, word_list, param1=None, hot_dict={}):
                         param1=after_n,
                         hot_dict=hot_dict)
 
-            hot_print(res, word_list, param1, hot_dict)
+            top = res.json()["data"].get("children")
+            for hot in top:
+                for hot_word in word_list:
+                    title = hot.get("data").get("title")
+                    if title is not None:
+                        words = title.split()
+                        for word in words:
+                            if hot_word.lower() == word.lower():
+                                hot_dict[hot_word] += 1
+
+            if param1 is None:
+                count = sorted(
+                    hot_dict.items(),
+                    key=lambda x: x[1],
+                    reverse=True)
+
+                for k, v in count:
+                    if v != 0:
+                        print(f'{k}: {v}')
+
             return hot_dict
 
         else:
-            hot_print(res, word_list, param1, hot_dict)
+            top = res.json()["data"].get("children")
+            for hot in top:
+                for hot_word in word_list:
+                    title = hot.get("data").get("title")
+                    if title is not None:
+                        words = title.split()
+                        for word in words:
+                            if hot_word.lower() == word.lower():
+                                hot_dict[hot_word] += 1
+
+            if param1 is None:
+                count = sorted(
+                    hot_dict.items(),
+                    key=lambda x: x[1],
+                    reverse=True)
+
+                for k, v in count:
+                    if v != 0:
+                        print(f'{k}: {v}')
+
             return hot_dict
     else:
         return None
