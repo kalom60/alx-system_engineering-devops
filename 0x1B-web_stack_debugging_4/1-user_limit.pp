52d047f07d8f5af7497698f1c7a402353c2aa7ef
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-05-06 01:18:49 +0300

    fix error

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -10,4 +10,4 @@ exec { 'increase-hard-file-limit':
 exec { 'increase-soft-file-limit':                                                                                                                                      
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',                                                                                           
   path    => '/usr/local/bin/:/bin/'                                                                                                                                    
-}
+} 
