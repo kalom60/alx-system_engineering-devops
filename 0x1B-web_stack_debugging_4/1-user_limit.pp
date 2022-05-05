186095a3386e30470328f59d632724bab924bde7
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-05-06 01:25:14 +0300

    fix error

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,12 @@
-# Enable the user holberton to login and open files without error.                                                                                                      
-                                                                                                                                                                        
-# Increase hard file limit for Holberton user.                                                                                                                          
-exec { 'increase-hard-file-limit':                                                                                                                                      
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',                                                                                           
-  path    => '/usr/local/bin/:/bin/'                                                                                                                                    
-}                                                                                                                                                                       
-                                                                                                                                                                        
-# Increase soft file limit for Holberton user.                                                                                                                          
-exec { 'increase-soft-file-limit':                                                                                                                                      
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',                                                                                           
-  path    => '/usr/local/bin/:/bin/'                                                                                                                                    
-} 
+# Fix problem of high amount files opened
+
+exec {'increase-hard-file-limit':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['increase-soft-file-limit'],
+}
+
+exec {'increase-soft-file-limit':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
